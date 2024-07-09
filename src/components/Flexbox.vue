4b26a93214ad6e67cf4d6010f873fdb01f05338f
RB <53470171+RBercasio22@users.noreply.github.com>
2024-07-08 20:56:14 -0700

    Update Flexbox.vue

--- src/components/Flexbox.vue
+++ src/components/Flexbox.vue
@@ -1,9 +1,6 @@
 <template>
     
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam illo nostrum cumque soluta corporis, ipsum labore nemo sed incidunt facere deserunt sint aspernatur eos ducimus aperiam dignissimos laboriosam quam aliquam. Veniam fuga quae nisi molestias eveniet. Adipisci, reprehenderit veritatis dicta assumenda perferendis, consectetur alias, eligendi laudantium sequi nobis ut consequuntur?</p>
-        <div>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, quae.</h1>
-        </div>
+       
 
 </template>
 
